6fb89a75cc46ef42e68d59acb80dc258489c776a
Louis Orleans <louis@orleans.io>
2018-06-06 00:34:28 -0700

    🚧✨ identify line item Texts

--- lib/index.ts
+++ lib/index.ts
@@ -41,9 +41,7 @@ export function parse_pdf(pdf_data: FullPdfData): Install {
   );
   console.log(line_item_numbers_indexes.map(i => pdf_texts[i].R[0].T));
 
-  const line_items_indexes = find_match_indexes(pdf_texts, /3PI-INSTALL-.+/i, {
-    multiple: true,
-  });
+  const line_items_indexes = find_match_indexes(pdf_texts, /3PI-INSTALL-.+/i);
   if (line_items_indexes.length !== line_item_numbers_indexes.length) {
     // TODO: evaluate if I really want to `throw` an error here.
     throw new Error(dedent`
@@ -58,10 +56,16 @@ export function parse_pdf(pdf_data: FullPdfData): Install {
 
   console.log(line_items_indexes.map(i => pdf_texts[i].R[0].T));
 
+  console.log(`Install info:`);
+  console.log(extract_install_info(pdf_data));
+
   const line_items: LineItem[] = [];
 
   for (const i in line_item_numbers_indexes) {
-    const name_short = pdf_texts[line_items_indexes[i]].R[0].T.replace('3PI-INSTALL-', '');
+    const name_short = pdf_texts[line_items_indexes[i]].R[0].T.replace(
+      '3PI-INSTALL-',
+      '',
+    );
     line_items.push({
       id: parseInt(pdf_texts[line_item_numbers_indexes[i]].R[0].T, 10),
       name_short,
@@ -100,6 +104,88 @@ export function parse_pdf(pdf_data: FullPdfData): Install {
 }
 
 /**
+ * Extracts all line item text from the "Installation Information & Notes" box
+ * @param pdf_data
+ * @returns Array of strings of line item notes
+ */
+function extract_install_info(pdf_data: FullPdfData): string[] {
+  // TODO: What about text that overflows the box?
+
+  const texts: TextBlock[] = [];
+  let overall_height: number = 0;
+
+  for (let i = 0; i < pdf_data.formImage.Pages.length; i++) {
+    const page = pdf_data.formImage.Pages[i];
+    // --Start-- Find install info box bounds
+    // Find "Installation Information & Notes" in Texts
+    const install_info_text =
+      page.Texts[
+        find_match_indexes(page.Texts, /Installation Information/i, {
+          multiple: false,
+        })[0]
+      ];
+    if (install_info_text == null) {
+      throw new Error(`Could not find "Installation Information" text`);
+    }
+    // Find Fill that surrounds "Installation Information & Notes"
+    const bounding_fill = page.Fills.reduce((val, fill) => {
+      if (box_contains_point(fill, install_info_text)) {
+        if (val == null || val.w * val.h > fill.w * fill.h) return val;
+        else return fill;
+      } else return val;
+    });
+    // console.log(bounding_fill);
+    // Find box of `VLines` and `HLines` of similar width to `bounding_fill`
+    const left = page.VLines.reduce((val, line) => {
+      return Math.abs(line.x - bounding_fill.x) <
+        Math.abs(val.x - bounding_fill.x)
+        ? line
+        : val;
+    }, page.VLines[0]);
+    const right = page.VLines.reduce((val, line) => {
+      return Math.abs(line.x - (bounding_fill.x + bounding_fill.w)) <
+        Math.abs(val.x - (bounding_fill.x + bounding_fill.w))
+        ? line
+        : val;
+    }, page.VLines[0]);
+    // console.log(left, right);
+    // Store box's bounds
+    const bounding_box: Box = {
+      x: left.x,
+      y: left.y + bounding_fill.h,
+      w: right.x - left.x,
+      h: left.l - bounding_fill.h,
+    };
+    // console.log(bounding_box);
+    // --End--
+
+    // --Start-- Filter for text contained in install info box
+    const page_texts = page.Texts.filter(text =>
+      box_contains_point(bounding_box, text),
+    );
+    console.log(page_texts.length);
+    // --End--
+
+    // --Start-- Merge each page's texts
+    overall_height += i > 0 ? pdf_data.formImage.Pages[i - 1].Height : 0;
+    texts.push(...page_texts.map(text => ({
+      ...text,
+      y: text.y + overall_height,
+    })));
+    // --End--
+  }
+
+
+  // --Start-- Split text up in to line items
+  const line_item_numbers_indexes = find_match_indexes(texts, /^00\d0$/);
+  console.log('line_item_numbers_indexes');
+  console.log(line_item_numbers_indexes);
+  // --End--
+
+  return [];
+}
+
+/**
  * Gets product name by abbreviation
  * Returns abbreviation if no matching name found
  * @param abbr Product abbreviation
@@ -122,6 +208,15 @@ function item_abbr_to_name(abbr: string): string {
     default:
       return abbr;
   }
+}
+
+function box_contains_point(box: Box, point: Point): boolean {
+  return (
+    box.x <= point.x &&
+    box.y <= point.y &&
+    box.x + box.w >= point.x &&
+    box.y + box.h >= point.y
+  );
 }
 
 export interface Install {
@@ -156,6 +251,15 @@ export interface Person {
   phones?: string[];
   emails?: string[];
 }
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface Box extends Point {
+  w: number;
+  h: number;
 }
 
 export type FullPdfData = PdfData & { path: string };
